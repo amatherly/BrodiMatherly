fe0664d6fda446eee719171e534c77cd8f2b95ad
BRODI MATHERLY <69179454+amatherly@users.noreply.github.com>
2023-06-03 12:10:36 -0700

    WIP on the main page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Add your JavaScript code here
+console.log('Hello, GitHub Pages!');
+
+// Add your JavaScript code here
+function viewPortfolio() {
+  alert("Portfolio section is under construction!");
+}
+
