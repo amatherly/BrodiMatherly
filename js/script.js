2c335e0523642672563009d85411a82cffeed548
BRODI MATHERLY <69179454+amatherly@users.noreply.github.com>
2023-06-05 14:57:33 -0700

    Testing video pop ups

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 console.log('Hello, GitHub Pages!');
 
 
+let clip = document.querySelector(".vid")
 
+
+clip.addEventListener("mouseover", function (e) {
+    clip.play();
+})
+
+/* Applying the mouse out event to pause the video */
+clip.addEventListener("mouseout", function (e) {
+    clip.pause();
+})
